74416a49771455b52448c497491cab7bb888892c
Artur Kappel <artur.kappel@poatek.com>
2023-03-22 11:00:53 -0300

    Initial Commit

---
+++ MarvelAppi/MarvelAppiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppiApp.swift
+//  MarvelAppi
+//
+//  Created by Artur Kappel on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
